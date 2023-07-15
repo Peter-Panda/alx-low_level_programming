a51651a51ecbe056bb5457ba8185650ba3cc84b7
Peter-Panda <abdelghnihamnar1@gmail.com>
2023-07-15 12:07:17 -0700

    print tebalphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
