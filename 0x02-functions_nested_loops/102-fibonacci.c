8e880e08d3ddbbe0dd8aa4d4b98119e3688c9fe1
Peter-Panda <abdelghnihamnar1@gmail.com>
2023-07-18 12:30:11 -0700

    50 fibo

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long count, i, j, k;
+
+	for (count = 0; count < 50; count++)
+	{
+		k = i + j;
+		i = j;
+		j = k;
+
+		printf("%lu", k);
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
