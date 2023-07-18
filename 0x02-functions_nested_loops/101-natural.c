c3ee807579d8299ce22ae488b888cd78babec993
Peter-Panda <abdelghnihamnar1@gmail.com>
2023-07-18 08:15:39 -0700

    update

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, result;
+
+	for (i = 0; i < 1024; i++)
+		if (i % 3 == 0 || i %  5 == 0)
+			result += i;
+	printf("%d\n", result);
+	return (0);
+}
