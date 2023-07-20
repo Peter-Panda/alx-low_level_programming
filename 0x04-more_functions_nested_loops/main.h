286c19f72b1dab1e3324eba9c94a423781962b0c
Peter-Panda <abdelghnihamnar1@gmail.com>
2023-07-20 07:42:21 -0700

    print numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,15 @@
 
 #include <stdio.h>
 #include <unistd.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+
+
+
+
+
 
 #endif
