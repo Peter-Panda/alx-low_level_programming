ca61deac034c831a332e793169f3de242118c978
Peter-Panda <abdelghnihamnar1@gmail.com>
2023-07-24 11:56:07 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - return len of string
+ * @s: string to count
+ * Return: len of s
+ */
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\0')
+		n++;
+
+	return n;
+}
